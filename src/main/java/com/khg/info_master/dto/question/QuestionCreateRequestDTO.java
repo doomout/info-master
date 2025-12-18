6678258a8875670c87cc17b33ed64bdb659193d6
khg <winquake3@hanmail.net>
2025-12-18 23:46:43 +0900

    DB에 맞게 문제 생성 DTO 변경

--- src/main/java/com/khg/info_master/dto/question/QuestionCreateRequestDTO.java
+++ src/main/java/com/khg/info_master/dto/question/QuestionCreateRequestDTO.java
@@ -15,13 +15,13 @@ public class QuestionCreateRequestDTO {
     @NotNull(message = "round는 필수입니다.")
     private Integer round;
 
-    @NotBlank(message = "subject는 비어 있을 수 없습니다.")
-    private String subject;
+    // @NotBlank(message = "subject는 비어 있을 수 없습니다.")
+    // private String subject;
 
     @NotNull(message = "문제 번호(number)는 필수입니다.")
     private Integer number;
 
-    @NotBlank(message = "문제 본문(questionText)은 비어 있을 수 없습니다.")
+    @NotBlank(message = "문제는 비어 있을 수 없습니다.")
     private String questionText;
 
     private String difficulty;   // 선택 값
