e0f2b044f2b5d3fb507c97440cd25f4d7bab856f
khg <winquake3@hanmail.net>
2025-12-31 19:01:35 +0900

    헤더 페이지 전면 수정(관리자 모드 표시)

--- info-master-front/src/components/Header.tsx
+++ info-master-front/src/components/Header.tsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { isAdmin } from "../config/admin";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header-inner">
-        <h1 className="logo">기술사 공부</h1>
+        <div className="logo-area">
+          <h1 className="logo">기술사 공부</h1>
+
+          {isAdmin && (
+            <span className="admin-badge">관리자 모드</span>
+          )}
+        </div>
 
         <nav className="nav">
           <Link to="/">홈</Link>
-          <Link to="/members">회원 관리</Link>
-          <Link to="/questions">문제 관리</Link>
-          <Link to="/tags">카테고리 관리</Link>
+
+          {isAdmin && (
+            <>
+              <Link to="/admin">관리자 홈</Link>
+              <Link to="/questions">문제 관리</Link>
+              <Link to="/tags">카테고리 관리</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
