47eadecd119aadb382626ddef05685be8d121242
khg <winquake3@hanmail.net>
2026-01-01 21:50:06 +0900

    문제 생성/수정이 관리자만 가능하게 수정

---
+++ info-master-front/src/pages/Admin/AdminQuestionCreatePage.tsx
@@ -0,0 +1,5 @@
+import QuestionForm from "../Questions/QuestionForm";
+
+export default function AdminQuestionCreatePage() {
+  return <QuestionForm mode="create" />;
+}
---
+++ info-master-front/src/pages/Admin/AdminQuestionEditPage.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { QuestionApi } from "../../api/QuestionApi";
+import QuestionForm from "../Questions/QuestionForm";
+import type { Question } from "../../types/Question";
+
+export default function AdminQuestionEditPage() {
+  const { id } = useParams();
+  const [question, setQuestion] = useState<Question | null>(null);
+
+  useEffect(() => {
+    QuestionApi.get(Number(id))
+      .then(res => setQuestion(res.data))
+      .catch(console.error);
+  }, [id]);
+
+  if (!question) return <div>Loading...</div>;
+
+  return <QuestionForm mode="edit" initialData={question} />;
+}
