c5049372d125c95539fcbf1c9c00afaba9c0c570
khg <winquake3@hanmail.net>
2026-01-05 22:08:45 +0900

    실행 안되던 버그 수정

--- src/main/java/com/khg/info_master/domain/Admin.java
+++ src/main/java/com/khg/info_master/domain/Admin.java
@@ -1,23 +1,24 @@
 package com.khg.info_master.domain;
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "admin")
 public class Admin {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 }
