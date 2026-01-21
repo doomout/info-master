61f7259ce8b7d1b786201b446e0fbe6c0fab75b7
khg <winquake3@hanmail.net>
2026-01-21 19:47:41 +0900

    로그인 안되던 버그 수정

--- info-master-front/src/api/AdminApi.ts
+++ info-master-front/src/api/AdminApi.ts
@@ -1,18 +1,14 @@
-import axios from "./axios";
-
-/* 전역 설정 (한 번만) */
-axios.defaults.baseURL = "http://localhost:8080";
-axios.defaults.headers.common["Content-Type"] = "application/json";
+import api from "./axios";
 
 const BASE_URL = "/admin";
 
 export const AdminApi = {
   login(data: { username: string; password: string }) {
-    return axios.post(`${BASE_URL}/login`, data);
+    return api.post(`${BASE_URL}/login`, data);
   },
 
   me() {
-    return axios.get(`${BASE_URL}/me`);
+    return api.get(`${BASE_URL}/me`);
   },
 
   logout() {
