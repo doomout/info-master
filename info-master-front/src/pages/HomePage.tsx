344058be3f0bd74a14c5c77c9fc269f81324215a
khg <winquake3@hanmail.net>
2026-01-24 07:29:42 +0900

    프론트 CI 테스트를 위한 수정

--- info-master-front/src/pages/HomePage.tsx
+++ info-master-front/src/pages/HomePage.tsx
@@ -5,14 +5,14 @@ export default function HomePage() {
   return (
     <main className="home">
       <section className="hero">
-        <h2>정보관리기술사 문제 학습</h2>
+        <h2>정보 관리 기술사 문제 학습</h2>
         <p>
           연도·회차·카테고리 기준으로<br />
           기술사 기출문제를 학습할 수 있습니다.
         </p>
 
         <Link to="/questions" className="btn-primary">
-          문제 풀러 가기 →
+          문제 확인하러 가기
         </Link>
       </section>
     </main>
